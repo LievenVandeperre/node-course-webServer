33e9503485c1d1d2ebd6919c6d6b486557ef495d
Lieven Vandeperre <lieven.vandeperre@gmail.com>
2019-01-27 19:24:50 +0100

    adding Projects page

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.get("/about", (req, res) => {
   });
 });
 
+app.get("/projects", (req, res) =>{
+    res.render('projects.hbs', {
+        pageTitle: "About Page"
+    });
+});
+
 app.get("/bad", (req, res) => {
   res.send({
     errorMessage: "there be errors, mon"
